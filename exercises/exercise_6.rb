2640e747927de87ffe0c9e4f25414b9a39503e31
nicmak <nicholas123mak@gmail.com>
2016-12-07 00:41:39 +0000

    Complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,33 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+puts @store1
+
+#I want to add an employee but at the same time associate
+#their id to the store_id
+#This employees is referring to the store_ids in the employee table
+
+# SYNTAX FOR CREATING ENTRIES WITH FOREIGN ID
+# @nameOfTableWhereForeignKeyCameFrom.CurrentTableThatYouWanttoAddStuffTo.create(HASH)
+
+@store1.employees.create(
+first_name:"Nick",
+last_name:"Mak",
+hourly_rate:1
+)
+@store1.employees.create(
+first_name:"Beth",
+last_name:"Johansin",
+hourly_rate:40
+)
+
+@store2.employees.create(
+first_name:"SOCKS",
+last_name:"Mupeets",
+hourly_rate:40
+)
+@store2.employees.create(
+first_name:"Sess",
+last_name:"Khan",
+hourly_rate:40
+)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,12 +1,39 @@
 require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
-require_relative './exercise_6'
+# require_relative './exercise_1'
+# require_relative './exercise_2'
+# require_relative './exercise_3'
+# require_relative './exercise_4'
+# require_relative './exercise_5'
+# require_relative './exercise_6'
 
 puts "Exercise 7"
 puts "----------"
+puts "Please input the name of store"
+StoreName = gets.chomp.to_s
+
+addInfo = Store.create(
+          name:StoreName,
+          annual_revenue: -1,
+          mens_apparel:nil,
+          womens_apparel:nil)
+
+puts addInfo.errors[:name] << "HElloWORLD"
+puts addInfo.errors[:annual_revenue].clear << "annual_revenue must be greater than or equal to 0"
+puts addInfo.errors[:mens_apparel].clear << "Not a boolean Answer"
+puts addInfo.errors[:womens_apparel].clear << "Not a boolean Answer"
+
+
+
+
+
+# addInfo.errors.add(:name,"You must write a name")
+# puts addInfo.errors[:annual_revenue] << "Revenue must be greater than 0"
+# puts addInfo.errors[:mens_apparel] << "Not a boolean answer"
+# puts addInfo.errors[:mens_apparel] << "Not a boolean answer"
+
+
+# 'annual_revenue must be greater than or equal to 0'
+# 'mens_apparel cannot be nil'
+# 'womens_apparel cannot be nil'
 
 # Your code goes here ...
